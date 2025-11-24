f116deebd749cf00a4ffa327f6118c4b4253f0e6
Ushasri Tallada <ushasri.tallada@gmail.com>
2025-11-24 07:39:20 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
-  output: 'standalone',
   images: {
     remotePatterns: [
       {
@@ -27,4 +25,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
