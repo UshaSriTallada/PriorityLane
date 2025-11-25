038b33a4a60fd6b2745c1165b76e76a267c3fd1d
Ushasri Tallada <ushasri.tallada@gmail.com>
2025-11-25 13:26:51 +0000

    Build fix

--- src/app/dashboard/[division]/page.tsx
+++ src/app/dashboard/[division]/page.tsx
@@ -1,18 +1,33 @@
 
+// import DivisionDashboardClient from './division-dashboard-client';
+
+// interface DivisionDashboardPageProps {
+//   params: {
+//     division: string;
+//   };
+// }
+
+// // This is now a Server Component. It fetches data or params on the server
+// // and passes them down to a Client Component.
+// export default function DivisionDashboardPage({ params }: DivisionDashboardPageProps) {
+//   const decodedDivision = decodeURIComponent(params.division);
+
+//   return (
+//     <DivisionDashboardClient divisionSlug={decodedDivision} />
+//   );
+// }
+
+
 import DivisionDashboardClient from './division-dashboard-client';
 
-interface DivisionDashboardPageProps {
+type Props = {
   params: {
     division: string;
   };
-}
+};
 
-// This is now a Server Component. It fetches data or params on the server
-// and passes them down to a Client Component.
-export default function DivisionDashboardPage({ params }: DivisionDashboardPageProps) {
+export default function DivisionDashboardPage({ params }: Props) {
   const decodedDivision = decodeURIComponent(params.division);
 
-  return (
-    <DivisionDashboardClient divisionSlug={decodedDivision} />
-  );
+  return <DivisionDashboardClient divisionSlug={decodedDivision} />;
 }
